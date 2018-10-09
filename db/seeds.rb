d914d9856c08ecdbc089225decba9e39f52ad5b2
Michal Kováč <kovac@essential-data.sk>
2018-10-09 10:12:05 +0200

    Scraping ways fixed #2

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,16 +20,62 @@ xml.css('node').each do |node|
   toilets = node.css('tag[k=toilets]').first.try{|tag| tag.attr('v') == 'yes'}
   shop = node.css('tag[k=shop]').first.try{|tag| tag.attr('v') == 'yes'}
   Station.create(
-             id: id,
-             lat: lat,
-             lng: lng,
-             brand: brand,
-             name: name,
-             operator: operator,
-             opening_hours: opening_hours,
-             toilets: toilets,
-             shop: shop
+      id: id,
+      lat: lat,
+      lng: lng,
+      brand: brand,
+      name: name,
+      operator: operator,
+      opening_hours: opening_hours,
+      toilets: toilets,
+      shop: shop
   )
+end
+
+post_data = '(
+  area[name="Slovensko"];
+  way["amenity"="fuel"](area);
+);
+out body;>;out skel qt;'
+
+require 'net/http'
+doc = Net::HTTP.post_form(URI('http://overpass-api.de/api/interpreter'), data: post_data).body
+xml = Nokogiri::XML(doc)
+xml.css('way').each do |way|
+  id = way.attr('id')
+  puts "Parsing #{id}"
+  brand = way.css('tag[k=brand]').first.try(:attr, 'v')
+  name = way.css('tag[k=name]').first.try(:attr, 'v')
+  operator = way.css('tag[k=operator]').first.try(:attr, 'v')
+  opening_hours = way.css('tag[k=opening_hours]').first.try(:attr, 'v')
+  toilets = way.css('tag[k=toilets]').first.try{|tag| tag.attr('v') == 'yes'}
+  shop = way.css('tag[k=shop]').first.try{|tag| tag.attr('v') == 'yes'}
+  node_count = 0
+  sum_lat = 0
+  sum_lng = 0
+  way.css('nd').each do |nd|
+    node = xml.css("node\##{nd.attr('ref')}").first
+    if node
+      sum_lat += node.attr('lat').to_f
+      sum_lng += node.attr('lon').to_f
+      node_count += 1
+    else
+      puts "Wrong nd ref #{nd.attr('ref')}"
+    end
+  end
+  if node_count > 0
+    Station.create(
+        id: id,
+        lat: sum_lat / node_count,
+        lng: sum_lng / node_count,
+        brand: brand,
+        name: name,
+        operator: operator,
+        opening_hours: opening_hours,
+        toilets: toilets,
+        shop: shop
+    )
+  end
 end
 
 
